4307c7c1f581be4886827c9330630500660c7742
won_chan <83020544+boychan112@users.noreply.github.com>
2023-04-17 16:16:51 +0900

    license

--- portfolio.js
+++ portfolio.js
@@ -2,12 +2,21 @@ $('.sujung').click(function(){
     $('.modal').fadeIn();
     $('.sujung-modal').fadeIn();
     $('.hongbo-modal').hide();
+    $('.license-modal').hide();
 })
 
 $('.hongbo').click(function(){
     $('.modal').fadeIn();
     $('.sujung-modal').hide();
     $('.hongbo-modal').fadeIn();
+    $('.license-modal').hide();
+})
+
+$('.license').click(function(){
+    $('.modal').fadeIn();
+    $('.sujung-modal').hide();
+    $('.hongbo-modal').hide();
+    $('.license-modal').fadeIn();
 })
 
 $('.close-modal').click(function(){
