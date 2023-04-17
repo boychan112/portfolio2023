04e3c30e02d42168b25c1baadc6089e9d17859a1
won_chan <83020544+boychan112@users.noreply.github.com>
2023-04-17 16:31:09 +0900

    m

--- portfolio.js
+++ portfolio.js
@@ -1,24 +1,24 @@
 $('.sujung').click(function(){
     $('.modal').fadeIn();
-    $('.sujung-modal').fadeIn();
-    $('.hongbo-modal').hide();
+    $('.sujungmodal').fadeIn();
+    $('.hongbomodal').hide();
     $('.licensemodal').hide();
 })
 
 $('.hongbo').click(function(){
     $('.modal').fadeIn();
-    $('.sujung-modal').hide();
-    $('.hongbo-modal').fadeIn();
+    $('.sujungmodal').hide();
+    $('.hongbomodal').fadeIn();
     $('.licensemodal').hide();
 })
 
 $('.license').click(function(){
     $('.modal').fadeIn();
-    $('.sujung-modal').hide();
-    $('.hongbo-modal').hide();
+    $('.sujungmodal').hide();
+    $('.hongbomodal').hide();
     $('.licensemodal').fadeIn();
 })
 
-$('.close-modal').click(function(){
+$('.closemodal').click(function(){
     $('.modal').fadeOut();
 })
